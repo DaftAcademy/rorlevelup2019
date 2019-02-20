6ffd46d22e616174112aad4f870db7525acac8c0
Snowman90 <woj.krzywda@gmail.com>
2019-02-20 20:46:16 +0100

    Done recruitment task

--- recruitment_task/converter.rb
+++ recruitment_task/converter.rb
@@ -1,5 +1,39 @@
+# frozen_string_literal: true
+
 require_relative 'price'
 
 class Converter
-  # TODO: Implement me
+  class InvalidPriceObject < StandardError
+    def message
+      'Argument must be instance of Price class'
+    end
+  end
+
+  CURRENCIES = {
+    eur: { pln: 4.32, usd: 1.13 },
+    usd: { pln: 3.81, eur: 0.88 },
+    pln: { eur: 0.23, usd: 0.26 }
+  }.freeze
+
+  def initialize(price)
+    @price = price
+    validate_price
+  end
+
+  def convert_to(currency)
+    raise Price::InvalidCurrency unless Price::SUPPORTED_CURRENCIES.include?(currency)
+
+    converted_price = if currency == @price.currency
+                        @price.amount
+                      else
+                        @price.amount * CURRENCIES[@price.currency][currency]
+                      end
+    converted_price.round 2
+  end
+
+  private
+
+  def validate_price
+    raise InvalidPriceObject unless @price.instance_of? Price
+  end
 end

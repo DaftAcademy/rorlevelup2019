26cc81348b32ee6887031343bde551e5d0e6e4cc
Jakub Horosz <kubahorosz95@gmail.com>
2019-02-24 19:20:25 +0100

    solved task

--- recruitment_task/converter.rb
+++ recruitment_task/converter.rb
@@ -1,5 +1,18 @@
 require_relative 'price'
 
 class Converter
-  # TODO: Implement me
+  CURRENCIES = {
+    usd: { pln: 3.81, usd: 1,    eur: 0.88 },
+    eur: { pln: 4.32, usd: 1.13, eur: 1    },
+    pln: { pln: 1,    usd: 0.26, eur: 0.23 }
+  }.freeze
+
+  def initialize(price)
+    @price = price
+  end 
+
+  def convert_to(currency)
+    (@price.amount * CURRENCIES[@price.currency][currency]).round(2)
+  end 
 end
+

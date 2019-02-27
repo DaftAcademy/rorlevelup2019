36d9dc293df1ef2c5405437586df702d0c17ef11
michal_wiacek <michal.wiacek@letmepay.com>
2019-02-28 00:45:27 +0100

    potworek

--- recruitment_task/converter.rb
+++ recruitment_task/converter.rb
@@ -1,5 +1,53 @@
 require_relative 'price'
 
 class Converter
-  # TODO: Implement me
+  def initialize price
+    @price = price
+  end
+
+  def convert_to currency
+    case @price.currency
+    when :eur
+      case currency
+      when :pln
+        amount = @price.amount * 4.32
+      when :usd
+        amount = @price.amount * 1.13
+      when :eur
+        amount = @price.amount
+      else
+        raise StandardError unless Price::SUPPORTED_CURRENCIES.include?(currency)
+      end
+    when :usd
+      case currency
+      when :pln
+        amount = @price.amount * 3.81
+      when :eur
+        amount = @price.amount * 0.88
+      when :usd
+        amount = @price.amount
+      else
+        raise StandardError unless Price.SUPPORTED_CURRENCIES.include?(currency)
+      end
+    when :pln
+      case currency
+      when :eur
+        amount = @price.amount * 0.23
+      when :usd
+        amount = @price.amount * 0.26
+      when :pln
+        amount = @price.amount
+      else
+        raise StandardError unless Price.SUPPORTED_CURRENCIES.include?(currency)
+      end
+    end
+    puts amount.round(2)
+  end
 end
+
+price_in_euro = Price.new(10, :eur)
+converter = Converter.new(price_in_euro)
+converter.convert_to(:usd) # => 11.3
+converter.convert_to(:eur) # => 10
+converter.convert_to(:pln) # => 43.2
+converter.convert_to(:xxx)

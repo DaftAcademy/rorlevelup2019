84fcfd9c4e31db5b00578a1a9f5a364ec4d5a911
Jakub Strebeyko <strebeyko@gmx.com>
2019-02-27 20:13:22 +0100

    implement converter class

--- recruitment_task/converter.rb
+++ recruitment_task/converter.rb
@@ -1,5 +1,21 @@
 require_relative 'price'
 
 class Converter
-  # TODO: Implement me
+  def initialize(price)
+    @price = price
+    @exchange = {
+      eur2pln: 4.32,
+      eur2usd: 1.13,
+      usd2pln: 3.81,
+      usd2eur: 0.88,
+      pln2eur: 0.23,
+      pln2usd: 0.26
+  }.freeze
+  end
+
+  def convert_to(targetCurrency)
+    # binding.pry
+    rate = @price.currency == targetCurrency ? 1 : @exchange["#{@price.currency}2#{targetCurrency}".to_sym]
+    sprintf('%.2f', @price.amount * rate)
+  end
 end

5e3210bcaf68ab29ce282d712c9eff1d827410e5
tojawawel <bgawel93@gmail.com>
2019-03-01 09:47:52 +0100

    add convert currency funcionality

--- recruitment_task/converter.rb
+++ recruitment_task/converter.rb
@@ -1,5 +1,30 @@
 require_relative 'price'
 
 class Converter
-  # TODO: Implement me
+  class InvalidCurrency < StandardError; end
+
+  EXCHANGE = { eur: {pln: 4.32, usd: 1.13 },
+               usd: {pln: 3.81, eur: 0.88 },
+               pln: {eur: 0.23, usd: 0.26 }
+             }
+  attr_reader :price
+
+  def initialize(price)
+    @price = price
+  end
+
+  def convert_to(new_currency)
+    validate_currency(new_currency)
+    different_currency?(price, new_currency) ? (price.amount * EXCHANGE[price.currency][new_currency]).round(2) : (price.amount).round(2)
+  end
+
+  private
+
+  def validate_currency(new_currency)
+    raise InvalidCurrency unless EXCHANGE.keys.include?(new_currency)
+  end
+
+  def different_currency?(price, new_currency)
+    price.currency != new_currency
+  end
 end

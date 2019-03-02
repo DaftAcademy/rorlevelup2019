6f0bbe790df44856b0476fe7fb04bcc73000ac85
ozel981 <ozel981@interia.pl>
2019-03-03 00:20:41 +0100

    zad0

--- recruitment_task/converter.rb
+++ recruitment_task/converter.rb
@@ -1,5 +1,34 @@
 require_relative 'price'
 
 class Converter
-  # TODO: Implement me
+
+    attr_reader :price
+
+    SUPPORTED_CURRENCIES = %i[eur usd pln].freeze
+
+    RATE = { 
+        eur:{eur: 1,pln: 4.32,usd: 1.13}, 
+        usd:{usd: 1,pln: 3.81,eur: 0.88},
+        pln:{pln: 1,eur: 0.23,usd: 0.26}
+    }.freeze
+
+    def initialize(price)
+        @price = price
+        validate_price
+    end
+    
+    def convert_to(currency)
+        validate_currency(currency)
+        (@price.amount*RATE[@price.currency][currency]).round(2)
+    end    
+
+    private
+
+    def validate_currency(currency)
+        raise InvalidCurrency unless SUPPORTED_CURRENCIES.include?(currency)
+    end
+
+    def validate_price
+        raise InvalidCurrency unless @price.is_a?(Price)
+    end
 end

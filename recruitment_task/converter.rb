c05d95dd6819d46e032b4246f281ae24acdb8179
karolasp <44696351+karolasp@users.noreply.github.com>
2019-03-05 21:39:48 +0100

    Zadanie rekrutacyjne

--- recruitment_task/converter.rb
+++ recruitment_task/converter.rb
@@ -1,5 +1,33 @@
 require_relative 'price'
 
 class Converter
-  # TODO: Implement me
+  class NotSupportedCurrencyError < StandardError
+  end
+
+  EXCHANGE_RATES = {
+    eur: { pln: 4.32, eur: 1.00, usd: 1.13 },
+    usd: { pln: 3.81, eur: 0.88, usd: 1.00 },
+    pln: { pln: 1.00, eur: 0.23, usd: 0.26 }
+  }.freeze
+
+  SUPPORTED_CURRENCIES = %i[eur usd pln].freeze
+
+  attr_reader :price
+
+  def initialize(price)
+    @price = price
+  end
+
+  def convert_to(currency_to_convert)
+    validate_currency currency_to_convert
+    new_value = @price.amount*EXCHANGE_RATES[@price.currency][currency_to_convert]
+    # jeśli ma być round
+    new_value.round(2)
+    # jeśli mają być dwa miejsca po przecinku
+    # "%.2f" % new_value
+  end
+
+  def validate_currency(currency)
+    raise NotSupportedCurrencyError unless SUPPORTED_CURRENCIES.include?(currency)
+  end
 end

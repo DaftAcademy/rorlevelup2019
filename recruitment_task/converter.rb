eee93fd87dadd7c1f477969a2e01b7e734e8d4ca
Tomasz Skupiński <tomasz.skupinski@chimeraprime.com>
2019-03-05 06:37:17 +0100

    Handle converting currency

--- recruitment_task/converter.rb
+++ recruitment_task/converter.rb
@@ -1,5 +1,34 @@
 require_relative 'price'
 
 class Converter
-  # TODO: Implement me
+  class InvalidCurrencyError < StandardError; end
+
+  def initialize(price)
+    @price = price
+  end
+
+  def convert_to(currency)
+    validate_currency(currency)
+    (exchange_rate(currency) * exchange_amount).round(2)
+  end
+
+  private
+
+  EXCHANGE_RATES = {
+    eur: { pln: 4.32, usd: 1.13, eur: 1 },
+    usd: { pln: 3.81, eur: 0.88, usd: 1 },
+    pln: { eur: 0.23, usd: 0.26, pln: 1 }
+  }.freeze
+
+  def validate_currency(currency)
+    raise InvalidCurrencyError unless EXCHANGE_RATES.key?(currency)
+  end
+
+  def exchange_rate(currency)
+    EXCHANGE_RATES[@price.currency][currency]
+  end
+
+  def exchange_amount
+    @price.amount
+  end
 end

3025551bd0171dcdff2d8d2978f766760b24bc18
Piotr Czajkowski <czajkowskipp@gmail.com>
2019-03-06 18:50:25 +0100

    solved recruitment task

--- recruitment_task/converter.rb
+++ recruitment_task/converter.rb
@@ -1,5 +1,33 @@
 require_relative 'price'
 
 class Converter
-  # TODO: Implement me
+  class InvalidPrice < StandardError; end
+  class InvalidCurrency < StandardError; end
+
+  EXCHANGE_RATE = {
+    eur: { pln: 4.32, usd: 1.13 },
+    usd: { pln: 3.81, eur: 0.88 },
+    pln: { eur: 0.23, usd: 0.26 }    
+  }
+
+  def initialize(price)
+    @price = price
+    validate_price
+  end
+
+  def convert_to(currency)
+    @currency = currency
+    validate_currency
+    multiplier = (@price.currency == currency ? 1 : EXCHANGE_RATE[@price.currency][currency])
+    (@price.amount*multiplier).round(2)
+  end
+
+  def validate_price
+    raise InvalidPrice unless @price.instance_of? Price 
+  end
+
+  def validate_currency
+    raise InvalidCurrency unless %i[eur usd pln].include?(@currency)
+  end
 end
+

40f13cd12e685e4327aca85d549dcdf9a21bdc04
JakubBilski <47048420+JakubBilski@users.noreply.github.com>
2019-03-06 19:43:52 +0100

    Update converter.rb

--- recruitment_task/converter.rb
+++ recruitment_task/converter.rb
@@ -3,22 +3,17 @@ require_relative 'price'
 class Converter
   class InvalidCurrency < StandardError; end
   SUPPORTED_CURRENCIES = %i[eur usd pln].freeze
-  FROM_EUR = {:usd => 1.13, :pln => 4.32}
-  FROM_USD = {:eur => 0.88, :pln => 3.81}
-  FROM_PLN = {:eur => 0.23, :usd => 0.26}
+  RATE =
+  {
+      :eur => {:usd => 1.13, :pln => 4.32},
+      :usd => {:eur => 0.88, :pln => 3.81},
+      :pln => {:eur => 0.23, :usd => 0.26}
+  }
 
   def initialize(original_price)
     @price = original_price
-    case @price.currency
-    when :usd
-      @courses = FROM_USD
-    when :eur
-      @courses = FROM_EUR
-    when :pln
-      @courses = FROM_PLN
-    else
-      raise InvalidCurrency
-    end
+    raise InvalidCurrency unless SUPPORTED_CURRENCIES.include?(@prize.currency)
+    @courses = RATE[@prize.currency]
   end
   def convert_to(currency)
     raise InvalidCurrency unless SUPPORTED_CURRENCIES.include?(currency)

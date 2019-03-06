6e96317c3d728d750bcf9a87d538cbfc562e6533
ndebiec <ndebiec@wp.pl>
2019-03-06 16:56:03 +0100

    Added working converter

--- recruitment_task/converter.rb
+++ recruitment_task/converter.rb
@@ -1,5 +1,29 @@
 require_relative 'price'
 
 class Converter
-  # TODO: Implement me
+  attr_reader :input_price
+
+  def initialize(input_price)
+    @input_price = input_price
+  end
+  
+  POS = {eur: 0, usd: 1, pln: 2}
+  RATES = [
+    [1, 1.13, 4.32],
+    [0.88, 1, 3.81],
+    [0.23, 0.26, 1]
+  ]
+
+  def exchange_rate(from, to)
+    row = POS[from] 
+    col = POS[to]  
+    RATES[row][col]
+  end
+
+  def convert_to(target_curr)
+    raise InvalidCurrency.new unless POS.keys.include?(target_curr)
+    rate = exchange_rate(input_price.currency, target_curr)
+    input_val = input_price.amount.to_f
+    sprintf('%.2f',(rate * input_val))
+  end
 end

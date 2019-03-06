ee4cfb0a53a780451f32269e051e0c9f8f0afe84
norbx <norsku27@yahoo.com>
2019-03-06 15:56:43 +0100

    Add Converter

--- recruitment_task/converter.rb
+++ recruitment_task/converter.rb
@@ -1,5 +1,46 @@
 require_relative 'price'
 
-class Converter
-  # TODO: Implement me
+class Converter < Price
+  class InvalidObject < StandardError; end
+
+  def initialize(price)
+    validate_object(price)
+    @amount = price.amount
+    @currency = price.currency
+  end
+
+  def convert_to(currency)
+    validate_currency(currency)
+    amount = @amount * exchange_rate(@currency, currency)
+    amount.round(2)
+  end
+
+  private
+
+  def exchange_rate(from, to)
+    exchange_table = {
+      eur: {
+        pln: 4.32,
+        eur: 1,
+        usd: 1.13
+      },
+      usd: {
+        pln: 3.81,
+        eur: 0.88,
+        usd: 1
+      },
+      pln: {
+        pln: 1,
+        eur: 0.23,
+        usd: 0.26
+      }
+    }
+
+    exchange_table[from][to]
+  end
+
+  def validate_object(object)
+    raise InvalidObject unless object.instance_of?(Price)
+  end
+
 end

e06d2434de4ab1191ab33ef57db47be7c3b96519
gitpetrova <piotr.wydrzycki@yahoo.com>
2019-03-06 17:58:27 -0500

    converter class added

--- recruitment_task/converter.rb
+++ recruitment_task/converter.rb
@@ -1,5 +1,13 @@
 require_relative 'price'
 
 class Converter
-  # TODO: Implement me
+	RATES = { "EUR/PLN" => 4.32, "EUR/USD" => 1.13, "USD/PLN" => 3.81, "USD/EUR" => 0.88, "PLN/EUR" => 0.23, "PLN/USD" => 0.26 }
+
+	def initialize(price)
+		@price = price
+	end
+	def convert_to(convert_currency)
+		(@price.amount * RATES[[ @price.currency.upcase, convert_currency.upcase].join("/")]).round(2)
+
+	end
 end

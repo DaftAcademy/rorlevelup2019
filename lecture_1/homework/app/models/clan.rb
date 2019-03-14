804560e422c86022d20a0189231294c9f4855c73
Snowman90 <woj.krzywda@gmail.com>
2019-03-14 19:11:31 +0100

    Add models validation

--- lecture_1/homework/app/models/clan.rb
+++ lecture_1/homework/app/models/clan.rb
@@ -1,3 +1,5 @@
 class Clan < ApplicationRecord
   has_many :samurais, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum: 256 }
 end
--- lecture_1/homework/app/models/samurai.rb
+++ lecture_1/homework/app/models/samurai.rb
@@ -1,3 +1,12 @@
 class Samurai < ApplicationRecord
   belongs_to :clan
+
+  validates :name, presence: true, length: { maximum: 256 }
+  validates :armor, presence: true,
+                    numericality: { greater_than_or_equal_to: 0,
+                                    less_than_or_equal_to: 1000 }
+  validates :battle_count, presence: true
+  validates :join_at, presence: true
+  validates :death_at, presence: true
+  validates :clan_id, presence: true
 end

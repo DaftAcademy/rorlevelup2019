036645cac8986de4c5dd704626c57b335a8c0276
Snowman90 <woj.krzywda@gmail.com>
2019-03-15 20:21:03 +0100

    Updated clan model validation

--- lecture_1/homework/app/models/clan.rb
+++ lecture_1/homework/app/models/clan.rb
@@ -1,5 +1,5 @@
 class Clan < ApplicationRecord
   has_many :samurais, dependent: :destroy
 
-  validates :name, presence: true, length: { maximum: 256 }
+  validates :name, presence: true, uniqueness: true, length: { maximum: 256 }
 end

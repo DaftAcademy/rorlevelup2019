d235d9bd26b1605be312919f8eec3d07195fc367
Bartosz Rackowski <Bartosz.Rackowski@vimn.com>
2019-03-17 20:51:07 +0100

    created controller for clans

---
+++ lecture_1/homework/app/controllers/clans_controller.rb
@@ -0,0 +1,42 @@
+class ClansController < ApplicationController
+  def show
+    render json: clan.to_json(only: %w[id name])
+  end
+
+  def index
+    clans = Clan.all
+    render json: clans.to_json(only: %w[id name])
+  end
+
+  def create
+    clan = Clan.new(clan_params)
+    if clan.save
+      render json: clan.to_json(only: %w[id name])
+    else
+      render json: { error: clan.errors }, status: 422
+    end
+  end
+
+  def update
+    if clan.update(clan_params)
+      render json: clan.to_json(only: %w[id name])
+    else
+      render json: { error: clan.errors }, status: 422
+    end
+  end
+
+  def destroy
+    clan.destroy!
+    head 204
+  end
+
+  private
+
+  def clan
+    @clan ||= Clan.find(params[:id])
+  end
+
+  def clan_params
+    params.permit(:name, :id)
+  end
+end

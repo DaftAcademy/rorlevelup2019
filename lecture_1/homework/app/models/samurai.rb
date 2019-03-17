7ccffd2f2b7e99a3438b8aa23b5fd9f84e17af00
Bartosz Rackowski <Bartosz.Rackowski@vimn.com>
2019-03-17 18:32:35 +0100

    added tests for models

--- lecture_1/homework/app/models/samurai.rb
+++ lecture_1/homework/app/models/samurai.rb
@@ -3,5 +3,5 @@ class Samurai < ApplicationRecord
   validates :name, presence: true
   validates :armor_quality, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 1000}
   scope :alive, -> { where('death_date is NULL')}
-  scope :death, -> { where('death_date is NOT NULL')}
+  scope :dead, -> { where('death_date is NOT NULL')}
 end

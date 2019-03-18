e754d848e99983173c56c4b82d14a56c3956ff7d
OfftheCode <offthecode@gmail.com>
2019-03-18 19:19:35 +0100

    Question about routing

--- lecture_1/homework/config/routes.rb
+++ lecture_1/homework/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
+  # Czy powinniśmy odrazu wersjonować api? tj. namespace :api do namespace :v1 do czy moze jako scope?
   resources :clans, only: [:index, :create] do
-    get 'dead', to: 'clans#dead'
+    get 'dead', to: 'clans#dead' # Wydaje mi się to nieco wygodniejsze od parametru w indexie, ale zaimplementowałem oba rozwiązania
     resources :samurais 
   end
 

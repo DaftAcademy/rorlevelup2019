94db4c623096144ef4795976262a2983ae833294
OfftheCode <offthecode@gmail.com>
2019-03-18 18:26:53 +0100

    FIX samurais controller not returning anything when no limit was specified\nnow the default is 5

--- lecture_1/homework/app/controllers/clans_controller.rb
+++ lecture_1/homework/app/controllers/clans_controller.rb
@@ -1,14 +1,20 @@
 class ClansController < ApplicationController
 
   def index
-    limit = params[:limit].to_i
-    clans = limit.nil? ? Clan.first(5) : Clan.first(limit) #FIXME: alternative static version if paremeter wasnt provided doest work [empty array is returned]
+    limit = params.has_key?(:limit) ? params[:limit].to_i : 5
+    clans = Clan.first(limit)
     render json: clans.to_json(only: %w[name id])
   end
 
   def create
     clan = Clan.create!(clan_params)
     render json: clan.to_json(only: %w[name]), status: 201
+  end
+
+  def dead
+    clan = Clan.find(params[:clan_id])
+    samurais = clan.samurais.where.not(deathtime: nil)
+    render json: samurais.to_json(only: %i[id name armour joined_at deathtime])
   end
 
   private

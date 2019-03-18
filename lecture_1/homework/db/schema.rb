025d576ed418cf9a0951f3e6e016c0465b3e84d6
ndebiec <ndebiec@wp.pl>
2019-03-18 22:40:54 +0100

    Added tables, models and routes

---
+++ lecture_1/homework/db/schema.rb
@@ -0,0 +1,31 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2019_03_18_210655) do
+
+  create_table "clans", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "warriors", force: :cascade do |t|
+    t.string "name"
+    t.integer "armour"
+    t.integer "battles"
+    t.integer "clan_id"
+    t.datetime "join_date"
+    t.datetime "death_date"
+    t.index ["clan_id"], name: "index_warriors_on_clan_id"
+  end
+
+end

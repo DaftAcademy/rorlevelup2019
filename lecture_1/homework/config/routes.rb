2ecf9a0bfb05be8509154f9b1a207f188a00afbf
jluczak <jluczaksas@gmail.com>
2019-03-18 18:28:40 +0100

    Add show action

--- lecture_1/homework/config/routes.rb
+++ lecture_1/homework/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   scope "/api" do
     resources :clans do
-      resources :samurais, only: %i[index create update destroy]
+      resources :samurais, only: %i[index create update destroy show]
     end
   end
 end

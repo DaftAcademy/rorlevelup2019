6c705fb2414f959b4aae4cec02c4f13380da5bb4
rupinski75 <rupinski.k@gmail.com>
2019-03-18 21:07:51 +0100

    Revert "converter implemented"
    This reverts commit d563465d99bd56f14174a7279b16181aefc2795f.

--- recruitment_task/converter.rb
+++ recruitment_task/converter.rb
@@ -1,18 +1,5 @@
 require_relative 'price'
 
 class Converter
-  def initialize(price)
-    @amount = price.amount
-    @currency = price.currency
-    @eur_to = { pln: '4.32', usd: '1.13', eur: '1' }
-    @usd_to = { pln: '4.81', usd: '1',    eur: '0.88' }
-    @pln_to = { pln: '1',    usd: '0.26', eur:'0.23' }
-  end
-
-  def convert_to(currency)
-    Price.new(@amount, currency)
-    exchange_rate = instance_variable_get("@#{@currency}_to")
-    (@amount*exchange_rate[currency].to_f).round(2)
-  end
+  # TODO: Implement me
 end
-

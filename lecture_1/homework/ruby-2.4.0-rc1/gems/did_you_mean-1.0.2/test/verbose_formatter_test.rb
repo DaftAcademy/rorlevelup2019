8b195735bd559b1a74930ff997c70806f99c8598
ndebiec <ndebiec@wp.pl>
2019-03-18 21:52:22 +0100

    Project setup

---
+++ lecture_1/homework/ruby-2.4.0-rc1/gems/did_you_mean-1.0.2/test/verbose_formatter_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class VerboseFormatterTest < Minitest::Test
+  def setup
+    @error = assert_raises(NoMethodError){ self.inspectt }
+  end
+
+  def test_message
+    assert_equal <<~MESSAGE.chomp, @error.message
+      undefined method `inspectt' for #{method(:to_s).super_method.call}
+
+          Did you mean? inspect
+       
+    MESSAGE
+  end
+end

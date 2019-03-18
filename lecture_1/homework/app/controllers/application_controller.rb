7872144c35dd6b1bf8cbb686bfc82a1838f06e4f
jluczak <jluczaksas@gmail.com>
2019-03-18 18:09:05 +0100

    404 and 422 status

--- lecture_1/homework/app/controllers/application_controller.rb
+++ lecture_1/homework/app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  def render_unprocessable_entity_response(exception)
+    render json: exception.record.errors, status: :unprocessable_entity
+  end
+
+  def render_not_found_response(exception)
+    render json: { error: exception.message }, status: :not_found
+  end
 end

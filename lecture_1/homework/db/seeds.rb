3bd395e67c17543ead958d54079a62a59321b63e
OfftheCode <offthecode@gmail.com>
2019-03-18 19:50:33 +0100

    Seeds inside db

--- lecture_1/homework/db/seeds.rb
+++ lecture_1/homework/db/seeds.rb
@@ -1,7 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+clans = Clan.create([
+  { name: "Stary Obóz"},
+  { name: "Nordmar"},
+  { name: "Warszawka"}
+])
+
+Samurai.create( name: "Bezimienny",
+                armour: 371,
+                battles_count: 18,
+                death_at: nil,
+                joined_at: '2001-03-15',
+                clan_id: Clan.first.id
+                )
+
+Samurai.create( name: "Ja-nusz",
+                armour: 21,
+                battles_count: 1,
+                death_at: nil,
+                joined_at: '1980-11-20',
+                clan_id: clans[2].id
+                )
+
+Samurai.create( name: "Org1",
+                armour: 100,
+                battles_count: 2,
+                death_at: '2019-03-15',
+                joined_at: '1994-08-28',
+                clan_id: clans[1].id
+                )
+
+Samurai.create( name: "Org2",
+                armour: 100,
+                battles_count: 2,
+                death_at: '2019-03-15',
+                joined_at: '1994-08-28',
+                clan_id: clans[1].id
+                )
+
+Samurai.create( name: "Org3",
+                armour: 100,
+                battles_count: 2,
+                death_at: '2019-03-15',
+                joined_at: '1994-08-28',
+                clan_id: clans[1].id
+                )

fde8c94256eb0e690c73721458fbf15e15789d8d
jjkazm <jjkazm@gmail.com>
2019-03-19 16:51:28 +0100

    Validate samurai model and test validations

--- lecture_1/homework/app/models/samurai.rb
+++ lecture_1/homework/app/models/samurai.rb
@@ -1,3 +1,7 @@
 class Samurai < ApplicationRecord
   belongs_to :clan
+  validates :name, presence: true, uniqueness: true
+  validates :armor, presence: true, numericality: { only_integer: true },
+              inclusion: { in:  0..1000 }
+  validates :battles_count,  presence: true, numericality: { only_integer: true }
 end
--- lecture_1/homework/spec/models/samurai_spec.rb
+++ lecture_1/homework/spec/models/samurai_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Samurai, type: :model do
    it { should belong_to(:clan) }
+   it { should validate_presence_of(:name) }
+   it { should validate_uniqueness_of(:name) }
+   it { should validate_presence_of(:armor) }
+   it { should validate_numericality_of(:armor).only_integer }
+   it { should validate_inclusion_of(:armor).in_range(0..1000) }
+   it { should validate_presence_of(:battles_count) }
+   it { should validate_numericality_of(:battles_count).only_integer }
 end

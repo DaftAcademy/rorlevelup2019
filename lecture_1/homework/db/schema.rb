84d77d4d9f953a0cd2af3d37331a7f9898e1d60f
jjkazm <jjkazm@gmail.com>
2019-03-19 15:38:09 +0100

    Create Samurai model and migration

--- lecture_1/homework/db/schema.rb
+++ lecture_1/homework/db/schema.rb
@@ -10,12 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_19_141425) do
+ActiveRecord::Schema.define(version: 2019_03_19_143603) do
 
   create_table "clans", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "samurais", force: :cascade do |t|
+    t.string "name"
+    t.integer "armor"
+    t.integer "battles_count"
+    t.date "join_date"
+    t.date "death_date"
+    t.integer "clan_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["clan_id"], name: "index_samurais_on_clan_id"
   end
 
 end

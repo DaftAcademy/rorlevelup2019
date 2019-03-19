b3d1b7e4bf1bc1f83bc8cf2a316d4778c59a91ef
jjkazm <jjkazm@gmail.com>
2019-03-19 17:21:12 +0100

    Implement 'alive' method

--- lecture_1/homework/app/models/samurai.rb
+++ lecture_1/homework/app/models/samurai.rb
@@ -4,4 +4,8 @@ class Samurai < ApplicationRecord
   validates :armor, presence: true, numericality: { only_integer: true },
               inclusion: { in:  0..1000 }
   validates :battles_count,  presence: true, numericality: { only_integer: true }
+
+  def alive?
+    death_date.nil?
+  end
 end

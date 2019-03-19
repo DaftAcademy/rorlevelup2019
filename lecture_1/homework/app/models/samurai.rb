d0748c6be36c3549fd02cda5494da3dfe89b1b1c
Greg <wilusz.gregory@gmail.com>
2019-03-19 23:05:51 +0100

    Improve and fix validation

--- lecture_1/homework/app/models/samurai.rb
+++ lecture_1/homework/app/models/samurai.rb
@@ -2,7 +2,9 @@ class Samurai < ApplicationRecord
     belongs_to :clan
 
     validates :name, presence: true, length: { maximum: 256 }
-    validates :armor, presence: true, numericality: { greater_than: 0, less_than_or_equal: 1000 }
+    validates :armor, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1000 }
+    validates :join_date, presence: true
+    validates :battles, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
     scope :dead, -> { where.not(death_date: nil) }
     scope :alive, -> { where(death_date: nil) } 

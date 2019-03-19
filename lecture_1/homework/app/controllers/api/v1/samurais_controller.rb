34732f1353ff53c0352029f8b22023aa15705730
Greg <wilusz.gregory@gmail.com>
2019-03-19 19:44:47 +0100

    Add scope enabling also to index dead or alive samurais

--- lecture_1/homework/app/controllers/api/v1/samurais_controller.rb
+++ lecture_1/homework/app/controllers/api/v1/samurais_controller.rb
@@ -2,23 +2,25 @@ module Api
   module V1
     class SamuraisController < ApplicationController  
       def index
-        render json: clan.samurais.to_json(only: %w[id name armor battles join_date death_date clan_id])
+        samurais = choose_samurais_group
+        
+        render json: samurais.to_json(only: samurai_traits)
       end
       
       def show
-        render json: samurai.to_json(only: %w[id name armor battles join_date death_date clan_id])
+        render json: samurai.to_json(only: samurai_traits)
       end
 
       def create
         samurai = clan.samurais.create!(samurai_params)
 
-        render json: samurai.to_json(only: %w[name armor battles join_date death_date clan_id]), status: 201
+        render json: samurai.to_json(only: samurai_traits), status: 201
       end
 
       def update
         samurai.update!(samurai_params)
 
-        render json: samurai.to_json(only: %w[name armor battles join_date death_date])
+        render json: samurai.to_json(only: samurai_traits)
       end
 
       def destroy
@@ -28,6 +30,14 @@ module Api
       end
 
       private
+
+      def choose_samurais_group
+        params[:dead].empty? ? clan.samurais : dead_or_alive
+      end
+
+      def dead_or_alive
+        params[:dead] == 'true' ? clan.samurais.dead : clan.samurais.alive
+      end
 
       def samurai
         @samurai ||= clan.samurais.find(params[:id])
@@ -39,6 +49,10 @@ module Api
 
       def samurai_params
         params.permit(:name, :armor, :battles, :join_date, :death_date, :clan_id)
+      end
+
+      def samurai_traits
+        %w[name armor battles join_date death_date clan_id]
       end
     end
   end
--- lecture_1/homework/app/models/samurai.rb
+++ lecture_1/homework/app/models/samurai.rb
@@ -3,4 +3,7 @@ class Samurai < ApplicationRecord
 
     validates :name, presence: true, length: { maximum: 256 }
     validates :armor, presence: true, numericality: { greater_than: 0, less_than_or_equal: 1000 }
+
+    scope :dead, -> { where.not(death_date: nil) }
+    scope :alive, -> { where(death_date: nil) } 
 end

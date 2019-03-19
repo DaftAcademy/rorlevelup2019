61be7819b8c33643101a210661c998b3b460d6a0
Patryk Kwaśniak <snade@outlook.com>
2019-03-19 21:01:58 +0100

    Remove samurai join date presence validation

--- lecture_1/homework/app/models/samurai.rb
+++ lecture_1/homework/app/models/samurai.rb
@@ -1,7 +1,7 @@
 class Samurai < ApplicationRecord
   belongs_to :clan
 
-  validates_presence_of :name, :armor_quality, :battle_count, :joined_at
+  validates_presence_of :name, :armor_quality, :battle_count
   validates_length_of :name, minimum: 2, maximum: 256
   validates_numericality_of :armor_quality, greater_than_or_equal_to: 0, less_than_or_equal_to: 1000
 end

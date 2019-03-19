d155c76c1cfd576cfd9143d7395237e96f986c77
jjkazm <jjkazm@gmail.com>
2019-03-19 17:20:21 +0100

    Set up routes

--- lecture_1/homework/config/routes.rb
+++ lecture_1/homework/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :clans, only: [:index, :new, :create] do
+    resources :samurais do
+      collection do
+        get 'dead'
+        get 'alive'
+      end
+    end
+  end
 end

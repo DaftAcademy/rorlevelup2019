e4567feb719e93ec38cbf8ded65cf71f0eba6b6f
Patryk Kwaśniak <snade@outlook.com>
2019-03-19 20:14:31 +0100

    Add clan and samurai specs

--- lecture_1/homework/spec/models/clan_spec.rb
+++ lecture_1/homework/spec/models/clan_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Clan, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:samurais).dependent(:destroy) }
+
+  it { should validate_presence_of(:name) }
+  it { should validate_length_of(:name).is_at_least(2).is_at_most(256) }
 end
--- lecture_1/homework/spec/models/samurai_spec.rb
+++ lecture_1/homework/spec/models/samurai_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Samurai, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:clan) }
+
+  it { should validate_presence_of(:name) }
+  it { should validate_length_of(:name).is_at_least(2).is_at_most(256) }
+  it { should validate_presence_of(:armor_quality) }
+  it { should validate_numericality_of(:armor_quality).is_greater_than_or_equal_to(0).is_less_than_or_equal_to(1000) }
+  it { should validate_presence_of(:battle_count) }
+  it { should validate_presence_of(:joined_at) }
+  it { should allow_value(nil).for(:died_at) }
 end

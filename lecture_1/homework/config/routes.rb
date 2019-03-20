23c3281965c12aeb9c707fc6a717174e62421b6a
tom.debek@gmail.com <tom.debek@gmail.com>
2019-03-20 14:30:07 +0100

    Add my routs

--- lecture_1/homework/config/routes.rb
+++ lecture_1/homework/config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/clans/clan_id/samurais_alive', to: 'samurais#alive'
+  get '/clans/clan_id/samurais_death', to: 'samurais#dead'
+
+
+  resources :clans, only: %i[index create] do
+    resources :samurais, only: %i[index show create update destroy]
+  end
+
+
 end

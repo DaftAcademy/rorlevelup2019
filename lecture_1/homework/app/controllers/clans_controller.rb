267aa70791b91c26850f9c87972e824818b5cd90
tom.debek@gmail.com <tom.debek@gmail.com>
2019-03-20 14:31:35 +0100

    Add my methods with controllers

--- lecture_1/homework/app/controllers/clans_controller.rb
+++ lecture_1/homework/app/controllers/clans_controller.rb
@@ -1,2 +1,29 @@
 class ClansController < ApplicationController
+  before_action :clan, only: [:show]
+
+  def index
+    clans = Clan.all
+    render json: clans.to_json(only: %i[id name])
+  end
+
+  def create
+    clan = Clan.create!(clan_params)
+    if clan.save
+      render json: clan.to_json(only: %i[id name]),
+             status: 201
+    else
+      render json: { errors: clan.errors }, status: 422
+    end
+  end
+
+
+  private
+  def clan
+    clan ||= Clan.find(clan_params[:id])
+  end
+
+  def clan_params
+    params.permit(:name, :id)
+  end
+
 end
--- lecture_1/homework/app/controllers/samurais_controller.rb
+++ lecture_1/homework/app/controllers/samurais_controller.rb
@@ -1,2 +1,68 @@
 class SamuraisController < ApplicationController
+  before_action :set_clan
+  before_action :samurai
+
+  def index
+    samurais = clan.samurais.all
+    render json: samurais.to_json(only: %i[id name armour battles])
+  end
+
+  def show
+    render json: samurai.to_json()
+  end
+
+  def create
+    clan ||= Clan.find(params[:clan_id])
+    samurai = clan.samurais.create(samurai_params)
+    if samurai.save
+      render json: samurai.to_json(only: %i[:id name armour]),
+             status: 201
+    else
+      render json: {errors: samurai.errors },
+             status: 422
+    end
+  end
+
+  def update
+    if samurai.update!(samurai_params)
+      render json: samurai.to_json(only: %i[id name armour]),
+             status: 201
+    else
+      render json: {errors: samurai.errors},
+             status: 422
+    end
+  end
+
+  def alive
+    samurais = clan.samurais.where('death_date = ?', 'null')
+    render json: samurais.to_json(only: %i[id name armour battles join_date])
+  end
+
+  def death
+    samurais = clan.samurais.where.not('death_date = ?', 'null')
+    render json: samurais.to_json(only: %i[id name armour battles join_date death_date])
+  end
+
+  def destroy
+    samurai.destroy!
+    render json: "You are deleted #{samurai[:name]} Samurai".to_json, status: 200
+  end
+
+
+
+  private
+
+  def set_clan
+    clan ||= Clan.find(params[:clan_id])
+  end
+
+  def samurai
+    clan ||= Clan.find(params[:clan_id])
+    samurai ||= clan.samurais.find(params[:id])
+  end
+
+  def samurai_params
+    params.permit(:name, :armour, :battles, :join_date, :death_date, :clan_id)
+  end
+
 end

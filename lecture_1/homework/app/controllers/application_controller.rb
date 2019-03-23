1cb723fc331d5e7fbf4c61d1bf65dacd6610fb52
Maxization <damian.opoka99@gmail.com>
2019-03-23 15:18:03 +0100

    Error control

--- lecture_1/homework/app/controllers/application_controller.rb
+++ lecture_1/homework/app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    render json: { message: exception.message }, status: 404
+  end
+
+  rescue_from ActiveRecord::RecordInvalid do |exception|
+    render json: { message: exception.message }, status: 422
+  end
 end

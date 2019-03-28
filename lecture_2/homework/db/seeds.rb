df0ac787fe2477393e0550d2328f365424352e9b
rupinski75 <rupinski.k@gmail.com>
2019-03-28 19:58:36 +0100

    add warrior names validation

--- lecture_2/homework/db/seeds.rb
+++ lecture_2/homework/db/seeds.rb
@@ -7,12 +7,16 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 clan_1 = Clan.create!(name: 'Blooming cherry')
+clan_2 = Clan.create!(name: 'Blooming apple')
 p '1 clan created'
 p Samurai.create!(name: 'Mietek', armor_quality: 100, join_date: Date.today, clan: clan_1, defensible: Castle.create)
-Samurai.create!(name: 'Mietek_ale_nieżywy', armor_quality: 23, join_date: Date.today, death_date: Date.today, clan: clan_1, defensible: Stronghold.create)
-Bowman.create!(name: 'Mietek_ale_z_rogalem', armor_quality: 75, join_date: Date.today, clan: clan_1, defensible: Gate.create)
-Canon.create!(name: 'Mietek_ale_armata', armor_quality: 75, join_date: Date.today, clan: clan_1, defensible: Gate.create)
-Hussar.create!(name: 'Mietek_ale_Zagłoba', armor_quality: 100, join_date: Date.today, clan: clan_1, defensible: Gate.create)
+p Samurai.create!(name: 'Mietek', armor_quality: 100, join_date: Date.today, clan: clan_2, defensible: Castle.create)
+# p Samurai.create!(name: 'Mietek', armor_quality: 100, join_date: Date.today, clan: clan_1, defensible: Castle.create)
+p Samurai.create!(name: 'Mietek_ale_nieżywy', armor_quality: 23, join_date: Date.today, death_date: Date.today, clan: clan_1, defensible: Stronghold.create)
+p Samurai.create!(name: 'Mietek_ale_nieżywy', armor_quality: 23, join_date: Date.today, clan: clan_1, defensible: Stronghold.create)
+p Bowman.create!(name: 'Mietek_ale_z_rogalem', armor_quality: 75, join_date: Date.today, clan: clan_1, defensible: Gate.create)
+p Canon.create!(name: 'Mietek_ale_armata', armor_quality: 75, join_date: Date.today, clan: clan_1, defensible: Gate.create)
+p Hussar.create!(name: 'Mietek_ale_Zagłoba', armor_quality: 100, join_date: Date.today, clan: clan_1, defensible: Gate.create)
 
 p clan_1.warriors.map(&:attack)
 # p 'army crated'

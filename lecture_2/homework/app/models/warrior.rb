3c6c226bb9438e0e7fff563fa4ab5ca4d727258e
Greg <wilusz.gregory@gmail.com>
2019-03-31 00:09:23 +0100

    Fix warrior's name model validation

--- lecture_2/homework/app/models/warrior.rb
+++ lecture_2/homework/app/models/warrior.rb
@@ -3,7 +3,9 @@ class Warrior < ApplicationRecord
     belongs_to :defensible, polymorphic: true
     has_one :weapon, as: :attackable
 
-    validates :name, presence: true, uniqueness: { scope: 'death_date.present?', :clan_id }
+    validates :name, presence: true, uniqueness: { scope: :clan_id }, if: Proc.new { |it| it.death_date.blank? }, on: :update
+    validates :name, presence: true, uniqueness: { scope: :clan_id }, unless: Proc.new { |it| it.death_date.blank? }, on: :create
+    
     validates :armor_quality, numericality: { only_integer: true,
                                             greater_than_or_equal_to: 0,
                                             less_than_or_equal_to: 100 }

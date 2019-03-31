16c3898e17a11d2879f43db518649862c22a9620
OfftheCode <offthecode@gmail.com>
2019-03-31 13:52:20 +0200

    Implement Defensible, warriors can now be part of defence building

--- lecture_2/homework/db/schema.rb
+++ lecture_2/homework/db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_31_113253) do
+ActiveRecord::Schema.define(version: 2019_03_31_114701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "archers", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "barricades", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -32,6 +37,16 @@ ActiveRecord::Schema.define(version: 2019_03_31_113253) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "strongholds", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "towers", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "warriors", force: :cascade do |t|
     t.string "name", null: false
     t.integer "armor_quality", default: 0
@@ -42,7 +57,10 @@ ActiveRecord::Schema.define(version: 2019_03_31_113253) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "type", default: "Samurai"
+    t.string "defensible_type"
+    t.bigint "defensible_id"
     t.index ["clan_id"], name: "index_warriors_on_clan_id"
+    t.index ["defensible_type", "defensible_id"], name: "index_warriors_on_defensible_type_and_defensible_id"
   end
 
   add_foreign_key "warriors", "clans"

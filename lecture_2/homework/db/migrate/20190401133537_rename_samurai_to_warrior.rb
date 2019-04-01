5d1b470df2824e70171ef0ad6db43c3c6c0065ca
tom.debek@gmail.com <tom.debek@gmail.com>
2019-04-01 15:38:47 +0200

    Change my samurais to warriors :)

---
+++ lecture_2/homework/db/migrate/20190401133537_rename_samurai_to_warrior.rb
@@ -0,0 +1,6 @@
+class RenameSamuraiToWarrior < ActiveRecord::Migration[5.2]
+  def change
+    rename_table :samurais, :warriors
+    add_column :warriors, :type, :string, default: 'Samurai'
+  end
+end

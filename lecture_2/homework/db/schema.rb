a64c1e6909d0c2c71a989fe501a9e96f84a246f7
jluczak <jluczaksas@gmail.com>
2019-04-01 12:50:50 +0200

    Add validations on Clan name

--- lecture_2/homework/db/schema.rb
+++ lecture_2/homework/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_14_150921) do
+ActiveRecord::Schema.define(version: 2019_04_01_102200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_03_14_150921) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_clans_on_name", unique: true
   end
 
   create_table "samurais", force: :cascade do |t|

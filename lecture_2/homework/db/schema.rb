9d18be33bbf162c5ea68256a6617f66a00c985f7
tom.debek@gmail.com <tom.debek@gmail.com>
2019-04-01 16:36:36 +0200

    rails g migration AddIndexNameToClans name:string:index

--- lecture_2/homework/db/schema.rb
+++ lecture_2/homework/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_01_134944) do
+ActiveRecord::Schema.define(version: 2019_04_01_143516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_04_01_134944) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_clans_on_name"
   end
 
   create_table "warriors", force: :cascade do |t|

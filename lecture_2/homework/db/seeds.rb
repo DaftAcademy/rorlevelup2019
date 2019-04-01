345b38dc33462b5e5c1af277596f5d6cdf6d813a
tom.debek@gmail.com <tom.debek@gmail.com>
2019-04-01 15:15:43 +0200

    Adding several clans with db seed

--- lecture_2/homework/db/seeds.rb
+++ lecture_2/homework/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+require 'faker'
+
+10.times do
+  Clan.create!(name: Faker::Games::ElderScrolls.race)
+end

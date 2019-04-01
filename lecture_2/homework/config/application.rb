cb4e2a12841391df93edf948189b800f18153de4
tom.debek@gmail.com <tom.debek@gmail.com>
2019-04-01 15:03:21 +0200

    Add serialize to my app

--- lecture_2/homework/config/application.rb
+++ lecture_2/homework/config/application.rb
@@ -17,10 +17,13 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+ActiveModelSerializers.config.adapter = :json_api
+
 module Lecture1
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    ActiveModelSerializers.config.adapter = :json_api
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers

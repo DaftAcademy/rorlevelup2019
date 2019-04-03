041fdb7b0c64094a193f3405908f9bb9ea742a53
OfftheCode <offthecode@gmail.com>
2019-04-03 10:45:06 +0200

    Add response module to handle errors

--- lecture_2/homework/app/controllers/application_controller.rb
+++ lecture_2/homework/app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
-  
+
+  include Response
   include ExceptionHandler
 
 end
---
+++ lecture_2/homework/app/controllers/concerns/response.rb
@@ -0,0 +1,7 @@
+module Response
+
+  def json_response(object, status = :ok)
+    render json: object, status: status
+  end
+
+end

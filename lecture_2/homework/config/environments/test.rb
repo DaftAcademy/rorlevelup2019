195826ee9339c29181a9ada93e4801208ce9648c
szoroh <kubahorosz95@gmail.com>
2019-04-03 12:31:18 +0200

    Add name uniqueness to clan

--- lecture_2/homework/config/environments/test.rb
+++ lecture_2/homework/config/environments/test.rb
@@ -7,6 +7,11 @@ Rails.application.configure do
   # and recreated between test runs. Don't rely on the data there!
   config.cache_classes = true
 
+  RSpec.configure do |config|
+      config.include(Shoulda::Matchers::ActiveModel, type: :model)
+        config.include(Shoulda::Matchers::ActiveRecord, type: :model)
+    end
+
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.

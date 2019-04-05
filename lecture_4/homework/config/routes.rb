48035bf92c81c2691e41d50d0711f9e937828f94
Maciej Głowacki <maciej.glowacki@daftcode.pl>
2019-04-05 12:21:32 +0200

    feat(Lecture-4): Add new homework

--- lecture_4/homework/config/routes.rb
+++ lecture_4/homework/config/routes.rb
@@ -4,4 +4,14 @@ Rails.application.routes.draw do
   resources :clans, only: %i[index create show] do
     resources :warriors, module: :clans
   end
+
+  resources :mercenaries, only: %i[index show] do
+    collection do
+      post 'employ_best'
+    end
+
+    member do
+      post 'employ'
+    end
+  end
 end

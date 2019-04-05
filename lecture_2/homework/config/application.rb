fae7d455efd96399f890be8852154800aed3b260
Snowman90 <woj.krzywda@gmail.com>
2019-04-05 17:39:44 +0200

    Update gitignore

--- lecture_2/homework/config/application.rb
+++ lecture_2/homework/config/application.rb
@@ -1,26 +1,28 @@
+# frozen_string_literal: true
+
 require_relative 'boot'
 
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "action_cable/engine"
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'action_cable/engine'
 # require "sprockets/railtie"
-require "rails/test_unit/railtie"
+require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 ActiveModelSerializers.config.adapter = :json_api
-
+Dotenv::Railtie.load
 Raven.configure do |config|
-  config.dsn = 'https://1f46e8ba0ab54ed59fb66e82706bc62e:53c7482d3645408384f8dd384dcb21bb@sentry.io/1428297'
+  config.dsn = ENV.fetch('SENTRY_DSN')
 end
 
 module Lecture1

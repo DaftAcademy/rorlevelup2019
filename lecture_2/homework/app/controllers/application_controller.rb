ddc7b9d8f2b036372c946fb617ca643f169f1349
Greg <wilusz.gregory@gmail.com>
2019-04-06 09:45:38 +0200

    Setup Sentry

--- lecture_2/homework/app/controllers/application_controller.rb
+++ lecture_2/homework/app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::API
   include Errorable
+
+  before_action :set_raven_context
+
+  private
+
+  def set_raven_context
+    Raven.user_context(id: session[:current_user_id])
+    Raven.extra_context(params: params.to_unsafe_h, url: request.url)
+  end
 end

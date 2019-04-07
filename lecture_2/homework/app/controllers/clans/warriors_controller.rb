7578cc100689b27d690ac8e9bf646addabca1ddc
rupinski75 <rupinski.k@gmail.com>
2019-04-07 12:40:15 +0200

    Finish fast json:api

--- lecture_2/homework/app/controllers/clans/warriors_controller.rb
+++ lecture_2/homework/app/controllers/clans/warriors_controller.rb
@@ -3,33 +3,30 @@
 module Clans
   class WarriorsController < ApplicationController
     def show
-      json_response(warrior)
+      json_response(WarriorSerializer.new(warrior).serialized_json)
     end
 
     def index
       warriors = clan.warriors
       if params.key?(:alive)
-        if params[:alive].to_i.zero?
-          json_response(warriors.dead)
-        else
-          json_response(warriors.alive)
+        if params[:alive].to_i.zero? # clans/1/warriors?alive=0
+          json_response(WarriorSerializer.new(warriors.dead).serialized_json)
+        else # clans/1/warriors?alive=1
+          json_response(WarriorSerializer.new(warriors.alive).serialized_json)
         end
       else
-        json_response(warriors)
+        json_response(WarriorSerializer.new(warriors).serialized_json)
       end
     end
 
     def create
       warrior = clan.warriors.create!(warrior_params)
-
-      render json: warrior.to_json, status: 201
-      json_response(warrior, 201)
+      json_response(WarriorSerializer.new(warrior).serialized_json, 201)
     end
 
     def update
       warrior.update!(warrior_params)
-
-      json_response(warrior)
+      json_response(WarriorSerializer.new(warrior).serialized_json)
     end
 
     def destroy
--- lecture_2/homework/app/controllers/clans_controller.rb
+++ lecture_2/homework/app/controllers/clans_controller.rb
@@ -1,12 +1,17 @@
+# frozen_string_literal: true
+
 class ClansController < ApplicationController
   def index
-    json_response(Clan.all)
+    # json_response(Clan.all)
+    options = {}
+    options[:include] = [:warriors]
+    json_response(ClanSerializer.new(Clan.all, options).serialized_json)
   end
 
   def create
     clan = Clan.create!(clan_params)
 
-    json_response(clan, 201)
+    json_response(ClanSerializer.new(clan).serialized_json, 201)
   end
 
   private

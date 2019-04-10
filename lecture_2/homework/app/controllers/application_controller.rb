a0f3060f268d94d123cd73fdec8805f64e054701
szoroh <kubahorosz95@gmail.com>
2019-04-10 16:42:54 +0200

    Add error handling logic to ApplicationController

--- lecture_2/homework/app/controllers/application_controller.rb
+++ lecture_2/homework/app/controllers/application_controller.rb
@@ -1,11 +1,5 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
-  rescue_from ActiveRecord::RecordNotFound do |exception|
-    render json: { message: exception.message }, status: 404
-  end
-
-  rescue_from ActiveRecord::RecordInvalid do |exception|
-    render json: { message: exception.message }, status: 422
-  end
+  include ErrorsHandler
 end

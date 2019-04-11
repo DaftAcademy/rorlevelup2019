0eb4829a94a5580a2a49596c79d05ae2b6e169a5
ndebiec <ndebiec@wp.pl>
2019-04-11 15:01:31 +0200

    Added scopes to name validation

--- lecture_2/homework/app/models/warrior/warrior.rb
+++ lecture_2/homework/app/models/warrior/warrior.rb
@@ -2,11 +2,12 @@ class Warrior < ApplicationRecord
   belongs_to :clan
   belongs_to :defensible, polymorphic: true
 
-  validates :name, presence: true, uniqueness: {scope: :clan_id}
+  validates :name, presence: true, uniqueness: {scope: :clan_id, conditions: -> { where(death_date: nil) },
+  							 , message: "There can't be 2 warriors with the same name in a clan" }
   validates :armor_quality, numericality: { only_integer: true,
                                             greater_than_or_equal_to: 0,
                                             less_than_or_equal_to: 100 }
 
-  scope :alive, -> { where('death_date IS NULL') }
-  scope :dead, -> { where('death_date IS NOT NULL') }
+  scope :alive, -> { where(death_date: nil) }
+  scope :dead, -> { where.not(death_date: nil) }
 end

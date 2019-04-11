e0c695fcbe4a3414e892a5c9c7e6d86b8c6fa9dc
jluczak <jluczaksas@gmail.com>
2019-04-11 12:54:50 +0200

    Add Sentry

--- lecture_2/homework/app/controllers/application_controller.rb
+++ lecture_2/homework/app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  before_action :set_raven_context
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
@@ -11,4 +12,11 @@ class ApplicationController < ActionController::API
   def render_not_found_response(exception)
     render json: { error: exception.message }, status: :not_found
  end
+
+ private
+
+ def set_raven_context
+  Raven.user_context(id: session[:current_user_id]) # or anything else in session
+  Raven.extra_context(params: params.to_unsafe_h, url: request.url)
+end
 end

38b4e39283f8b8a3cfd64e6b1ffeb5ebb8a6f021
ndebiec <ndebiec@wp.pl>
2019-04-13 10:32:01 +0200

    Extracted error handling to an active support concern module

--- lecture_2/homework/app/controllers/application_controller.rb
+++ lecture_2/homework/app/controllers/application_controller.rb
@@ -1,12 +1,3 @@
 class ApplicationController < ActionController::API
-  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
-
-  def render_not_found_response(exception)
-    render json: { message: exception.message }, status: 404
-  end
-
-  def render_unprocessable_entity_response(exception)
-    render json: { message: exception.message, errors: exception.record.errors }, status: 422
-  end
+  include Errorable
 end

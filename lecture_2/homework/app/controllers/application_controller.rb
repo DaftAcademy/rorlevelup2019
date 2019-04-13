1a7b903627bf2d676bf55570dcb08059286851f1
ndebiec <ndebiec@wp.pl>
2019-04-13 10:01:37 +0200

    Added record handling methods to application controller

--- lecture_2/homework/app/controllers/application_controller.rb
+++ lecture_2/homework/app/controllers/application_controller.rb
@@ -1,9 +1,12 @@
 class ApplicationController < ActionController::API
-  rescue_from ActiveRecord::RecordNotFound do |exception|
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+  def render_not_found_response(exception)
     render json: { message: exception.message }, status: 404
   end
 
-  rescue_from ActiveRecord::RecordInvalid do |exception|
-    render json: { message: exception.message }, status: 422
+  def render_unprocessable_entity_response(exception)
+    render json: exception.record.errors, status: 422
   end
 end

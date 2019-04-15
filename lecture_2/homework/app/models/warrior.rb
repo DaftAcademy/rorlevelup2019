ca5087a30d6bc310d15344d28a8990b89991e82e
Patryk Kwaśniak <snade@outlook.com>
2019-04-15 21:35:08 +0200

    Refactor warrior conditions and scopes

--- lecture_2/homework/app/models/warrior.rb
+++ lecture_2/homework/app/models/warrior.rb
@@ -6,12 +6,12 @@ class Warrior < ApplicationRecord
   has_one :weapon
 
   validates :name, presence: true, uniqueness: { scope: :clan_id,
-                                                 conditions: -> { where('death_date IS NULL') } }
+                                                 conditions: -> { where(death_date: nil) } }
   validates :armor_quality, numericality: { only_integer: true,
                                             greater_than_or_equal_to: 0,
                                             less_than_or_equal_to: 100 }
   validates :weapon_kind, presence: true, inclusion: { in: Weapon.kinds.keys }
 
-  scope :alive, -> { where('death_date IS NULL') }
-  scope :dead, -> { where('death_date IS NOT NULL') }
+  scope :alive, -> { where(death_date: nil) }
+  scope :dead, -> { where.not(death_date: nil) }
 end

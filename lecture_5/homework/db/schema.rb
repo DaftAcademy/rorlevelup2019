076f3018ab9d0148b8e3036df06aea424c733969
Snowman90 <woj.krzywda@gmail.com>
2019-04-15 18:27:42 +0200

    Add siege_ability column to Building model

--- lecture_5/homework/db/schema.rb
+++ lecture_5/homework/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_14_111449) do
+ActiveRecord::Schema.define(version: 2019_04_15_162214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_04_14_111449) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "granary", default: 0, null: false
+    t.integer "siege_ability", default: 0
   end
 
   create_table "clans", force: :cascade do |t|

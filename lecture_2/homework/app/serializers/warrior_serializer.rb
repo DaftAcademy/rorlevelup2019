76a828f544d9025d89beb5fcb21e8215ff933332
Patryk Kwaśniak <snade@outlook.com>
2019-04-15 21:50:36 +0200

    Add missing death_date attribute to warrior serializer
    Refactor code

--- lecture_2/homework/app/serializers/warrior_serializer.rb
+++ lecture_2/homework/app/serializers/warrior_serializer.rb
@@ -3,7 +3,7 @@
 class WarriorSerializer
   include FastJsonapi::ObjectSerializer
 
-  attributes :name, :armor_quality, :number_of_battles, :join_date, :type, :weapon_kind
+  attributes :name, :armor_quality, :number_of_battles, :join_date, :death_date, :type, :weapon_kind
   belongs_to :clan
   belongs_to :defensible
   has_one :weapon
--- lecture_2/homework/app/serializers/weapon_serializer.rb
+++ lecture_2/homework/app/serializers/weapon_serializer.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class WeaponSerializer
   include FastJsonapi::ObjectSerializer
 

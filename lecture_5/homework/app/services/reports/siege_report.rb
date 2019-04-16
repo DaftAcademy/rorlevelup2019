3d6be4bfa2f87da75aaa4f0f53d2f535d8327af3
s14990 <s14990@pjwstk.edu.pl>
2019-04-16 08:46:45 -0700

    forgot about rubocop

--- lecture_5/homework/app/services/reports/siege_report.rb
+++ lecture_5/homework/app/services/reports/siege_report.rb
@@ -3,13 +3,12 @@
 module Reports
   class SiegeReport
     def self.call(building:, warrior_id: nil)
-      
       # this should stop the method if getting null
       return unless building
 
       # bulding cant be defended without warriors
       if building.warriors.where.not(id: warrior_id).any?
-        # ignores that warrior whose id it got 
+        # ignores that warrior whose id it got
         warriors_count = building.warriors.where(type: 'Warriors::Samurai').where.not(id: warrior_id).count
         hussar_count = building.warriors.where(type: 'Warriors::Hussar').where.not(id: warrior_id).count
         total_rice_need = warriors_count + hussar_count * 2 + building.default_rice_need
--- lecture_5/homework/spec/helpers/reports/siege_report_spec.rb
+++ lecture_5/homework/spec/helpers/reports/siege_report_spec.rb
@@ -10,14 +10,14 @@ RSpec.describe Reports::SiegeReport, type: :helper do
     it 'set building siege_value to be 250' do
       w1 = create(:warrior, :samurai, clan: clan, building: building)
       w2 = create(:warrior, :samurai, clan: clan, building: building)
-      #Reports::SiegeReport.call(building: building)
+      # Reports::SiegeReport.call(building: building)
       expect(building.siege_ability).to eq(250)
     end
 
     it 'set building siege_value to be 500 after one warrior was destroyed' do
       w1 = create(:warrior, :samurai, clan: clan, building: building)
       w2 = create(:warrior, :samurai, clan: clan, building: building)
-      #Reports::SiegeReport.call(building: building)
+      # Reports::SiegeReport.call(building: building)
       w2.destroy!
       expect(building.siege_ability).to eq(500)
     end
@@ -25,7 +25,7 @@ RSpec.describe Reports::SiegeReport, type: :helper do
     it 'set building siege_value to be 0 after both warriors was destroyed' do
       w1 = create(:warrior, :samurai, clan: clan, building: building)
       w2 = create(:warrior, :samurai, clan: clan, building: building)
-      #Reports::SiegeReport.call(building: building)
+      # Reports::SiegeReport.call(building: building)
       w1.destroy!
       w2.destroy!
       expect(building.siege_ability).to eq(0)
@@ -34,7 +34,7 @@ RSpec.describe Reports::SiegeReport, type: :helper do
     it 'set building siege_value to be 250 after building change' do
       w1 = create(:warrior, :samurai, clan: clan, building: building)
       w2 = create(:warrior, :samurai, clan: clan, building: building2)
-      #Reports::SiegeReport.call(building: building)
+      # Reports::SiegeReport.call(building: building)
       w2.update!(building: building)
       w2.save!
       expect(building.siege_ability).to eq(250)
@@ -43,7 +43,7 @@ RSpec.describe Reports::SiegeReport, type: :helper do
     it 'set building siege_value to be 250 after building change' do
       w1 = create(:warrior, :samurai, clan: clan, building: building)
       w2 = create(:warrior, :samurai, clan: clan, building: building)
-      #Reports::SiegeReport.call(building: building)
+      # Reports::SiegeReport.call(building: building)
       w2.update!(building: building2)
       w2.save!
       expect(building.siege_ability).to eq(250)

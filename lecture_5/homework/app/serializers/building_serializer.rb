68a0951df46bf26066b388482aab275abe3b9e3a
Snowman90 <woj.krzywda@gmail.com>
2019-04-16 18:19:10 +0200

    Update building serializer

--- lecture_5/homework/app/serializers/building_serializer.rb
+++ lecture_5/homework/app/serializers/building_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class BuildingSerializer < ActiveModel::Serializer
-  attributes :name, :type, :granary
+  attributes :name, :type, :granary, :siege_ability
 
   has_many :warriors
 end

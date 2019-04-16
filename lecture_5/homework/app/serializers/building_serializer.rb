6b104042573f4d2b7c1b26e8a447e0a314ef6ba7
Michal <ninnja@wp.pl>
2019-04-16 02:04:51 +0200

    Zadanie 5

--- lecture_5/homework/app/serializers/building_serializer.rb
+++ lecture_5/homework/app/serializers/building_serializer.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class BuildingSerializer < ActiveModel::Serializer
-  attributes :name
+  attributes :name, :granary, :siege_ability
 
   has_many :warriors
+
+  def siege_ability
+    Reports::SiegeReport.new(building: object).call
+  end
 end

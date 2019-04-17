c2f4d55ed748b37110e513a050974e6f286f9f08
Maciej Janusz Krajsman <mjkrajsman@gmail.com>
2019-04-17 13:40:22 +0200

    Modify routes

--- lecture_2/homework/config/routes.rb
+++ lecture_2/homework/config/routes.rb
@@ -3,8 +3,6 @@
 Rails.application.routes.draw do
   get 'clans/index'
   get 'characters/index'
-  get '/clans/:clan_id/alive', to: 'characters#alive'
-  get '/clans/:clan_id/dead', to: 'characters#dead'
   resources :clans, only: %i[index create show update destroy] do
     resources :characters, module: :clans
   end

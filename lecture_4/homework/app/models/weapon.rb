3e1737010f8c56472ecb4ddaadd992551c63a917
s14990 <s14990@pjwstk.edu.pl>
2019-04-17 06:14:54 -0700

    minor_fixes

--- lecture_4/homework/app/models/weapon.rb
+++ lecture_4/homework/app/models/weapon.rb
@@ -4,7 +4,6 @@ class Weapon < ApplicationRecord
   KINDS = %w[magical melee ranged].freeze
 
   belongs_to :warrior
-  validates :name, presence: true, uniqueness: true
   validates :range, numericality: { greater_than_or_equal_to: 0 }
   validates :damage, numericality: { greater_than_or_equal_to: 0 }
   validates :kind, inclusion: { in: KINDS }

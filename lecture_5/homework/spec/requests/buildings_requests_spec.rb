c0b3d12678bc6cf58ea1ec4f56887f110e81ad66
Bartosz Rackowski <Bartosz.Rackowski@vimn.com>
2019-04-17 11:10:02 +0200

    added tests

---
+++ lecture_5/homework/spec/requests/buildings_requests_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe BuildingsController, type: :request do
+
+  describe 'GET /buildings' do
+    it 'responds with 200' do
+      get '/buildings'
+      expect(response).to have_http_status(200)
+      expect(JSON.parse(response.body)["data"]).to be_empty
+    end
+  end
+
+  describe 'GET /buildings/:id' do
+    it 'response with 404' do
+      get '/buildings/999'
+      expect(response).to have_http_status(404)
+    end
+  end
+
+  describe 'GET /buildings/:id' do
+    let(:building) { create(:building) }
+
+    it 'response with 200' do
+      get "/buildings/#{building.id}"
+      expect(response).to have_http_status(200)
+    end
+  end
+
+end

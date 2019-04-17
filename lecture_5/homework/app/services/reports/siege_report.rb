ce609c4de106a6f03582fbaa2899737ce1d301ab
Bartosz Rackowski <Bartosz.Rackowski@vimn.com>
2019-04-17 10:20:36 +0200

    implemented siege report

--- lecture_5/homework/app/services/reports/siege_report.rb
+++ lecture_5/homework/app/services/reports/siege_report.rb
@@ -3,10 +3,23 @@
 module Reports
   class SiegeReport
     def initialize(building:)
+      @building = building
+      @warriors = building.warriors
     end
 
     def call
-      raise NotImprementedYet
+      @warriors.present? ? @building.siege_ability = @building.granary / food_demand : @building.siege_ability = 0
+      @building.save
+      @building.siege_ability
+    end
+
+    private
+
+    def food_demand
+      daily_basic = 10
+      samurais = @warriors.where(type: "Warriors::Samurai").count
+      hussars = @warriors.where(type: "Warriors::Hussar").count * 2
+      daily_basic + samurais + hussars
     end
   end
 end
